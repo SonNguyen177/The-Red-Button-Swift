188e55b56b818acd95b3524f71b2f4837a65323b
l800891 <l800891@me.com>
2016-03-16 13:38:17 -0400

    Initial Commit

---
+++ TaptheButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaptheButton
+//
+//  Created by Chenglin Liu on 3/16/16.
+//  Copyright © 2016 Chenglin Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
